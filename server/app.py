26d7f6bf758f7b7ad8f63860cb099a11fbea2f97
Cherise <nashpaiessy@gmail.com>
2023-10-10 23:30:03 +0300

    done

--- server/app.py
+++ server/app.py
@@ -1,55 +1,123 @@
-from flask import Flask, request, make_response, jsonify
-from flask_migrate import Migrate
-from flask_restful import Api
-from models import db, User, Pic
-# import app
+
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+from models import db, User, Pic, Artist, UserArt, Sketch, Art
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///art.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+CORS(app)  # Enable CORS for all routes
+from flask_migrate import Migrate
 
 migrate = Migrate(app, db)
 
-db.init_app(app)
-api = Api(app)
 
+db.init_app(app)
+
+# GET all users
+@app.route('/users', methods=['GET'])
+def get_users():
+    users = User.query.all()
+    user_list = [{'id': user.id, 'user_name': user.user_name, 'password': user.password} for user in users]
+    return jsonify(user_list)
+
+# POST a new user
 @app.route('/users', methods=['POST'])
 def create_user():
-    data = request.get_json() 
+    data = request.get_json()
     user_name = data.get('user_name')
     password = data.get('password')
-    
+
+    existing_user = User.query.filter_by(user_name=user_name).first()
+    if existing_user:
+        return jsonify({'error': 'User with this username already exists'}), 400
+
     user = User(user_name=user_name, password=password)
     db.session.add(user)
     db.session.commit()
+    return jsonify({'message': 'User created successfully'})
 
-    return jsonify({'message': 'User created successfully'}) 
-
-@app.route('/users', methods=['GET'])  
-def get_users():
-    users = User.query.all()
-    return jsonify([user.to_dict() for user in users])
-
-@app.route('/users/<int:user_id>/pics', methods=['POST'])  
-def create_pic(user_id): 
-    user = User.query.get(user_id) 
+# DELETE a user by ID
+@app.route('/users/<int:user_id>', methods=['DELETE'])
+def delete_user(user_id):
+    user = User.query.get(user_id)
     if not user:
-        return jsonify({'error': 'User not found'})
+        return jsonify({'error': 'User not found'}), 404
+    db.session.delete(user)
+    db.session.commit()
+    return jsonify({'message': 'User deleted'})
 
-    data = request.json
+# GET all pics
+@app.route('/pics', methods=['GET'])
+def get_pics():
+    pics = Pic.query.all()
+    pic_list = [{'id': pic.id, 'caption': pic.caption, 'image_url': pic.image_url, 'user_id': pic.user_id} for pic in pics]
+    return jsonify(pic_list)
+
+# POST a new pic for a user
+@app.route('/pics', methods=['POST'])
+def create_pic():
+    data = request.get_json()
+    user_id = data.get('user_id')
+
+    user = User.query.get(user_id)
+    if not user:
+        return jsonify({'error': 'User not found'}), 404
+
     pic = Pic(user_id=user_id)
     db.session.add(pic)
     db.session.commit()
-    return jsonify(pic.serialize())
+    return jsonify({'message': 'Pic created successfully'})
 
-@app.route('/users/<int:user_id>', methods=['DELETE'])  
-def delete_user(user_id):  
-    user = User.query.get(user_id)
-    if not user:
-        return jsonify({'error': 'User not found'})
-    db.session.delete(user)
+# PATCH (update) a pic by ID
+@app.route('/pics/<int:pic_id>', methods=['PATCH'])
+def update_pic(pic_id):
+    pic = Pic.query.get(pic_id)
+    if not pic:
+        return jsonify({'error': 'Pic not found'}), 404
+
+    data = request.get_json()
+    pic.caption = data.get('caption')
     db.session.commit()
-    return jsonify({'message': 'User deleted'})  
+    return jsonify({'message': 'Pic updated successfully'})
+
+# DELETE a pic by ID
+@app.route('/pics/<int:pic_id>', methods=['DELETE'])
+def delete_pic(pic_id):
+    pic = Pic.query.get(pic_id)
+    if not pic:
+        return jsonify({'error': 'Pic not found'}), 404
+    db.session.delete(pic)
+    db.session.commit()
+    return jsonify({'message': 'Pic deleted'})
+
+# GET all artists
+@app.route('/artists', methods=['GET'])
+def get_artists():
+    artists = Artist.query.all()
+    artist_list = [{'id': artist.id, 'name': artist.name, 'rating': artist.rating, 'price': artist.price} for artist in artists]
+    return jsonify(artist_list)
+
+# GET all user-arts
+@app.route('/user-arts', methods=['GET'])
+def get_user_arts():
+    user_arts = UserArt.query.all()
+    user_art_list = [{'id': user_art.id, 'rating': user_art.rating, 'user_id': user_art.user_id, 'artist_id': user_art.artist_id} for user_art in user_arts]
+    return jsonify(user_art_list)
+
+# GET all sketches
+@app.route('/sketches', methods=['GET'])
+def get_sketches():
+    sketches = Sketch.query.all()
+    sketch_list = [{'id': sketch.id, 'title': sketch.title, 'description': sketch.description, 'image_url': sketch.image_url} for sketch in sketches]
+    return jsonify(sketch_list)
+
+# GET all arts
+@app.route('/arts', methods=['GET'])
+def get_arts():
+    arts = Art.query.all()
+    art_list = [{'id': art.id, 'name': art.name, 'rating': art.rating, 'price': art.price} for art in arts]
+    return jsonify(art_list)
 
 if __name__ == '__main__':
     app.run(port=5000)
--- server/models.py
+++ server/models.py
@@ -1,48 +1,65 @@
-# class  User(db.M)
 from flask_sqlalchemy import SQLAlchemy
-# from sqlalchemy_serializer import SerializerMix
-from sqlalchemy import DateTime,func
-db=SQLAlchemy()
+from sqlalchemy import DateTime, func
+
+db = SQLAlchemy()
+
 class User(db.Model):
-    __tablename__='users'
-    id=db.Column(db.Integer,primary_key=True)
-    user_name=db.Column(db.String)
-    password=db.Column(db.String)
-
-
-    pics=db.relationship('Pic',backref='user')
-
-# class Pic(db.Model):
-#     __tablename__='pics'
-#     id=db.Column(db.Integer,primary_key=True)
-#     user_id=db.Column(db.Integer)
-#     caption=db.Column(db.String)
-#     image_url=db.Column(db.String)
-#     created_at = db.Column(DateTime, default=func.current_timestamp())
-#     user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
-#     users=db.relationship('User',back_populates='pics')
-
-class Art(db.model):
-    __tablename__ = 'artist'
-    id=db.Column(db.Integer,primary_key=True)
-    name= db.Column
-    created_at=db.Column(db.DateTime,server_default=db.func.now())
-    rating=db.Column(db.Integer)
-    price=db.Column(db.Integer)
-
-class Userart(db.Model):
-    __tablename__ = "user_arts"
-    id=db.Column
-    rating =db.Column(db.Integer,db.Foreign)
-    user_id=db.Column(db.Integer, db.ForeignKey=='users.id')
-    art_id = db.Column(db.Integer,db.ForeignKey=='arts.id')
-
+    __tablename__ = 'users'
     
+    id = db.Column(db.Integer, primary_key=True)
+    user_name = db.Column(db.String)
+    password = db.Column(db.String)
+
+    # Define a one-to-many relationship with Pic
+    pics = db.relationship('Pic', backref='user', lazy=True)
+
+class Pic(db.Model):
+    __tablename__ = 'pics'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    caption = db.Column(db.String)
+    image_url = db.Column(db.String)
+    created_at = db.Column(DateTime, default=func.current_timestamp(), nullable=False)
+
+    # Define a many-to-one relationship with User
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+
+class Artist(db.Model):
+    __tablename__ = 'artists'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    created_at = db.Column(DateTime, default=func.current_timestamp(), nullable=False)
+    rating = db.Column(db.Integer)
+    price = db.Column(db.Integer)
+
+class UserArt(db.Model):
+    __tablename__ = "user_arts"
+
+    id = db.Column(db.Integer, primary_key=True)
+    rating = db.Column(db.Integer)
+
+    # Define a many-to-one relationship with User
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    user = db.relationship('User', backref='user_arts')
+
+    # Define a many-to-one relationship with Artist
+    artist_id = db.Column(db.Integer, db.ForeignKey('artists.id'), nullable=False)
+    artist = db.relationship('Artist', backref='user_arts')
 
 class Sketch(db.Model):
-    __tablename__='sketchs'
-    id=db.Column(db.Integer,primary_key=True)
-    title=db.Column(db.String)
-    description=db.Column(db.String)
-    image_url=db.Column(db.String)
-    created_at = db.Column(DateTime, default=func.current_timestamp())
+    __tablename__ = 'sketches'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String)
+    description = db.Column(db.String)
+    image_url = db.Column(db.String)
+    created_at = db.Column(DateTime, default=func.current_timestamp(), nullable=False)
+class Art(db.Model):
+    __tablename__ = 'arts'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    created_at = db.Column(DateTime, default=func.current_timestamp(), nullable=False)
+    rating = db.Column(db.Integer)
+    price = db.Column(db.Integer)
